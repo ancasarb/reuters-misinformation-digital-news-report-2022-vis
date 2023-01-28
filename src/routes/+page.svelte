16a6c77e13e9a30387419fdcd536325e911a2dd0
Anca Sarb <sarb.anca@gmail.com>
2023-01-28 21:37:49 +0000

    add data

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import load from '../routes/api/api';
+</script>
+
+<main>
+	{#await load('/api/data')}
+		<p>loading</p>
+	{:then data}
+	{/await}
+</main>
+
+<style>
+	main {
+		font-family: 'Inter', sans-serif;
+	}
+</style>
---
+++ src/scripts/metadata.js
@@ -0,0 +1,20 @@
+export const regionColors = {
+	Europe: '#0ab1be',
+	'North America': '#ec691d',
+	Asia: '#b1105d',
+	'Latin America': '#0e1f4f',
+	Africa: '#8dc15b'
+};
+
+export const textColor = '#414140';
+
+export const regions = ['Europe', 'North America', 'Asia', 'Latin America', 'Africa'];
+
+export const topics = [
+	'COVID-19',
+	'Politics',
+	'Celebrities',
+	'Climate change',
+	'Immigration',
+	'Products (e.g. scams)'
+];
