d63b034e937083f2151f80d3a11c54ad2d10eb7b
Adrian Mroz <adrian.mroz@allegro.pl>
2023-02-21 18:10:25 +0100

    layers!

--- src/components/charts/horizontalgroupedbarchart/Bar.svelte
+++ src/components/charts/horizontalgroupedbarchart/Bar.svelte
@@ -1,85 +1,93 @@
 <script>
-	import { tweened } from 'svelte/motion';
-	import { cubicOut } from 'svelte/easing';
+  import { tweened } from 'svelte/motion';
+  import { cubicOut } from 'svelte/easing';
 
-	export let d;
+  export let d;
 
-	export let onSelect;
-	export let onReset;
-	export let selected;
+  export let onSelect;
+  export let onReset;
+  export let selected;
 
-	export let xAccessor;
-	export let xScale;
+  export let xAccessor;
+  export let xScale;
 
-	export let yAccessor;
-	export let yScale;
+  export let yAccessor;
+  export let yScale;
 
-	export let color;
-	export let format;
+  export let color;
+  export let format;
 
-	$: category = yAccessor(d);
-	$: value = xAccessor(d);
-	$: x = xScale(value);
-	$: y = yScale(category);
+  $: category = yAccessor(d);
+  $: value = xAccessor(d);
+  $: x = xScale(value);
+  $: y = yScale(category);
 
-	const valueOpacity = tweened(1, {
-		duration: 500,
-		easing: cubicOut
-	});
+  const valueOpacity = tweened(1, {
+    duration: 500,
+    easing: cubicOut
+  });
 
-	const textOpacity = tweened(1, {
-		duration: 500,
-		easing: cubicOut
-	});
+  const textOpacity = tweened(1, {
+    duration: 500,
+    easing: cubicOut
+  });
 
-	$: {
-		const isGreyedOut = !(category === selected || selected === '');
-		if (isGreyedOut) {
-			valueOpacity.set(0.125);
-			textOpacity.set(0);
-		} else {
-			valueOpacity.set(1);
-			textOpacity.set(1);
-		}
-	}
+  $: {
+    const isGreyedOut = !(category === selected || selected === '');
+    if (isGreyedOut) {
+      valueOpacity.set(0.125);
+      textOpacity.set(0);
+    } else {
+      valueOpacity.set(1);
+      textOpacity.set(1);
+    }
+  }
 
+  $: width = xScale.range()[1];
+  $: height = yScale.bandwidth();
 </script>
 
-<rect
-	x={0}
-	{y}
-	height={yScale.bandwidth()}
-	width={xScale.range()[1]}
-	fill="#ececec"
-	on:mouseover={onSelect}
-	on:mouseout={onReset}
-/>
-<rect
-	x={0}
-	{y}
-	height={yScale.bandwidth()}
-	width={x}
-	fill={color}
-	on:mouseover={onSelect}
-	on:mouseout={onReset}
-	opacity={$valueOpacity}
-/>
+<div style:position="absolute" style:height="{height}px" style:top="{y}px" style:width="{width}px">
+  <svg viewBox="0 0 {width} {height}">
+    <rect
+      x={0}
+      y={0}
+      height={height}
+      width={width}
+      fill="#ececec"
+      on:mouseover={onSelect}
+      on:mouseout={onReset}
+    />
+    <rect
+      x={0}
+      y={0}
+      height={height}
+      width={x}
+      fill={color}
+      on:mouseover={onSelect}
+      on:mouseout={onReset}
+      opacity={$valueOpacity}
+    />
+  </svg>
 
-<text
-	class="annotation"
-	{x}
-	dx={-40}
-	{y}
-	dy={4}
-	dominant-baseline="hanging"
-	on:mouseover={onSelect}
-	on:mouseout={onReset}
-	opacity={$textOpacity}>{value}{format}</text
->
+  <div
+    class="annotation"
+    style:right="{width-x}px"
+    on:mouseover={onSelect}
+    on:mouseout={onReset}
+    style:opacity="{$textOpacity}">
+    {value}{format}
+  </div>
+</div>
 
 <style>
-	.annotation {
-		font-weight: bold;
-		fill: white;
-	}
+    .annotation {
+        position: absolute;
+        margin-right: 0.5em;
+        top: 0;
+        font-size: 15px;
+        line-height: 23px;
+        font-weight: bold;
+        color: white;
+    }
 </style>
--- src/components/charts/horizontalgroupedbarchart/Chart.svelte
+++ src/components/charts/horizontalgroupedbarchart/Chart.svelte
@@ -1,92 +1,94 @@
 <script>
-	import ColorLegend from '../../peripherals/ColorLegend.svelte';
-	import ChartTitle from '../../peripherals/ChartTitle.svelte';
-	import ChartFootnote from '../../peripherals/ChartFootnote.svelte';
+  import ColorLegend from '../../peripherals/ColorLegend.svelte';
+  import ChartTitle from '../../peripherals/ChartTitle.svelte';
+  import ChartFootnote from '../../peripherals/ChartFootnote.svelte';
 
-	import BarCluster from './BarCluster.svelte';
-	import Bar from './Bar.svelte';
+  import BarCluster from './BarCluster.svelte';
+  import Bar from './Bar.svelte';
 
-	import Chart from '../../generic/Chart.svelte';
+  import Chart from '../../generic/Chart.svelte';
 
-	import { scaleBand, scaleLinear } from 'd3';
-	import lodash from 'lodash';
-	const filter = lodash.filter;
+  import { scaleBand, scaleLinear } from 'd3';
+  import lodash from 'lodash';
 
-	export let data;
-	export let format;
+  const filter = lodash.filter;
 
-	export let clusters;
-	export let bars;
-	export let colorScale;
+  export let data;
+  export let format;
 
-	export let valueAccessor;
-	export let barAccessor;
-	export let clusterAccessor;
+  export let clusters;
+  export let bars;
+  export let colorScale;
 
-	const dimensions = {
-		width: 720,
-		height: 1024,
-		margin: {
-			top: 20,
-			left: 100,
-			bottom: 50,
-			right: 0
-		}
-	};
+  export let valueAccessor;
+  export let barAccessor;
+  export let clusterAccessor;
 
-	dimensions.innerWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right;
-	dimensions.innerHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+  const dimensions = {
+    width: 720,
+    height: 1024,
+    margin: {
+      top: 20,
+      left: 100,
+      bottom: 50,
+      right: 0
+    }
+  };
 
-	$: xScaleClusters = scaleBand()
-		.domain(clusters)
-		.range([0, dimensions.innerHeight])
-		.paddingInner(0.2);
-	$: xScaleBars = scaleLinear().domain([0, 100]).range([0, dimensions.innerWidth]);
-	$: yScale = scaleBand().domain(bars).range([0, xScaleClusters.bandwidth()]).paddingInner(0.2);
+  dimensions.innerWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right;
+  dimensions.innerHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
 
-	let selected = '';
+  $: xScaleClusters = scaleBand()
+    .domain(clusters)
+    .range([0, dimensions.innerHeight])
+    .paddingInner(0.2);
+  $: xScaleBars = scaleLinear().domain([0, 100]).range([0, dimensions.innerWidth]);
+  $: yScale = scaleBand().domain(bars).range([0, xScaleClusters.bandwidth()]).paddingInner(0.2);
 
-	function onSelect(value) {
-		selected = value;
-	}
+  let selected = '';
+
+  function onSelect(value) {
+    selected = value;
+  }
 </script>
 
 <Chart>
-	<ChartTitle width={dimensions.width} />
+  <ChartTitle width={dimensions.width}/>
 
-	<ColorLegend width={dimensions.width} {colorScale} categories={bars} {selected} />
+  <ColorLegend width={dimensions.width} {colorScale} categories={bars} {selected}/>
 
-	<svg
-		viewBox="0,0,{dimensions.width},{dimensions.height}"
-		style="max-width: {dimensions.width}px;"
-	>
-		<g transform={`translate(0, ${dimensions.margin.top})`}>
-			{#each clusters as cluster}
-				{@const clusterData = filter(data, (item) => clusterAccessor(item) === cluster)}
+  <div
+    style:width="{dimensions.width}px"
+    style:height="{dimensions.height - dimensions.margin.top}px"
+    style:top="{dimensions.margin.top}px"
+    style:position="relative"
+  >
+    {#each clusters as cluster}
+      {@const clusterData = filter(data, (item) => clusterAccessor(item) === cluster)}
 
-				<g transform={`translate(0, ${xScaleClusters(cluster)})`}>
-					<BarCluster category={cluster}>
-						{#each clusterData as d}
-							<g transform={`translate(${dimensions.margin.left}, 0)`}>
-								<Bar
-									{d}
-									onSelect={() => onSelect(barAccessor(d))}
-									onReset={() => onSelect('')}
-									{selected}
-									xAccessor={valueAccessor}
-									xScale={xScaleBars}
-									yAccessor={barAccessor}
-									{yScale}
-									color={colorScale(barAccessor(d))}
-									{format}
-								/>
-							</g>
-						{/each}
-					</BarCluster>
-				</g>
-			{/each}
-		</g>
-	</svg>
+      <div
+        style:position="relative"
+        style:width="{dimensions.width}px"
+        style:height="{xScaleClusters.step()}px">
+        <BarCluster category={cluster} width={dimensions.margin.left}>
+          {#each clusterData as d}
+            <Bar
+              {d}
+              onSelect={() => onSelect(barAccessor(d))}
+              onReset={() => onSelect('')}
+              {selected}
+              xAccessor={valueAccessor}
+              xScale={xScaleBars}
+              yAccessor={barAccessor}
+              {yScale}
+              color={colorScale(barAccessor(d))}
+              {format}
+            />
+          {/each}
+        </BarCluster>
+      </div>
+    {/each}
+  </div>
 
-	<ChartFootnote width={dimensions.width} />
+  <ChartFootnote width={dimensions.width}/>
 </Chart>
